fe5623745fdc6b4cfdbdb85a0aee4a9d3ee27f57
alpha008 <zjxlove2017@163.com>
2020-12-19 02:15:32 +0800

    add cgi

--- fastcgi/fcgi_test.c
+++ fastcgi/fcgi_test.c
@@ -11,20 +11,55 @@ int main(int argc, char *argv[])
 	//阻塞等待并监听某个端口，等待Nginx将数据发过来
 	while( FCGI_Accept() >= 0 ) 
 	{
-        int len;
+
+		char* method = getenv("REQUEST_METHOD");
+
         printf("Content-type: text/html\r\n\r\n");
+		printf("method:%s\n", method);
 		//如果想得到数据，需要从stdin去读，实际上从Nginx上去读
 		//如果想上传数据，需要往stdout写，实际上是给Nginx写数据
+		char *contentLength = getenv("CONTENT_LENGTH");
+        int len;
+        char token[128] = {0};
+        if( contentLength == NULL )
+        {
+            len = 0;
+        }
+        else
+        {
+            len = atoi(contentLength); //字符串转整型
+        }
+
+        if (len <= 0)//没有登陆用户信息
+        {
+            printf("No data from standard input.<p>\n");
+            printf( "len = 0, No data from standard input\n");
+        }
+        else //获取登陆用户信息
+        {
+			char buf[4*1024] = {0};
+            int ret = 0;
+            ret = fread(buf, 1, len, stdin); //从标准输入(web服务器)读取内容
+			if(ret == 0)
+            {
+                printf("fread(buf, 1, len, stdin) err\n");
+                continue;
+            }
+            printf("buf = %s\n", buf);
+		}
+#if 0
+		printf("len = %s\n",len);
 		printf("Content-type: text/html\r\n");
 		printf("\r\n");
 		printf("<title>Fast CGI Hello!</title>");
 		printf("<h1>Fast CGI Hello!</h1>\n");
-		printf("<h1>Fast CGI Hello!</h1>");
+
 		printf("SERVER_NAME:%s <br>\n", getenv("SERVER_NAME"));
 		printf(" <i>SERVER_PORT:%s</i>\n", getenv("SERVER_PORT"));
 		printf(" <i>QUERY_STRING:%s</i>\n", getenv("QUERY_STRING"));
 		//SERVER_NAME：得到server的host名称
 		printf("Request number %d running on host <i>%s</i>\n", ++count, getenv("SERVER_NAME"));
+#endif
 	}
 	
 	return 0;
--- fastcgi/test.sh
+++ fastcgi/test.sh
@@ -3,3 +3,7 @@ ps -ef | grep fcgi_test | grep -v grep | awk '{print $2}' | xargs kill -9
 spawn-fcgi -a 127.0.0.1 -p 9002 -f  /home/ubuntu/fastcgi/fastcgi/fcgi_test
 #ps -ef | grep fcgi_test | grep -v fcgi_test | awk '{print $2}' | xargs kill -9
 #http://182.254.181.144:9000/fcgi_test.cgi  
+
+
+#使用该网址模拟post方法
+#https://getman.cn/?s=749801132f968ca578ea3a6bfdd3d299
